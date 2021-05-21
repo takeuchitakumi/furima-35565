4b5e13b816fa999b693d0a0a84f5ec401db92527
takeuchitakumi <tahaizhunei616@gmail.com>
2021-05-21 16:54:27 +0900

    Revert "Revert "商品情報削除機能""
    This reverts commit 7b059bedeb040b3fa69747b715836bf44a0f69ae.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :item_id, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :item_id, only: [:show, :edit, :update, :destroy]
   before_action :ensure_current_user, only: [:edit, :update]
   
   def index
@@ -30,6 +30,12 @@ class ItemsController < ApplicationController
     if @item.update(item_params)
       redirect_to item_path(@item.id),method: :get
     else render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
     end
   end
 
